6f715c21fec834fc49e713cecd6b0d6e8cffa6df
Redor <matthieu.redor@hotmail.fr>
2018-06-11 14:40:18 +0200

    Initialisation du TP 2.1 Test Unitaire

---
+++ src/main/java/tp/testUnitaire/demo/testUnitaire/App.java
@@ -0,0 +1,13 @@
+package tp.testUnitaire.demo.testUnitaire;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
